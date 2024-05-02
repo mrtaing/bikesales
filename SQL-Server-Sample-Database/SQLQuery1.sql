5d7703b858cd2d1ab6ec985a1840d57666d76839
mrtaing <38343441+mrtaing@users.noreply.github.com>
2024-05-02 13:48:00 -0700

    Add files via upload

---
+++ SQL-Server-Sample-Database/SQLQuery1.sql
@@ -0,0 +1,29 @@
+SELECT
+	ord.order_id,
+	CONCAT(cus.first_name, ' ', cus.last_name) AS 'customer',
+	cus.city,
+	cus.state,
+	ord.order_date,
+	SUM(ite.quantity) AS 'total_units',
+	SUM(ite.quantity * ite.list_price) AS 'revenue',
+	pro.product_name,
+	cat.category_name,
+	sto.store_name,
+	CONCAT(sta.first_name, ' ', sta.last_name) AS 'sales_rep'
+FROM sales.orders ord
+JOIN sales.customers cus ON ord.customer_id = cus.customer_id
+JOIN sales.order_items ite ON ord.order_id = ite.order_id
+JOIN production.products pro ON pro.product_id = ite.product_id
+JOIN production.categories cat ON cat.category_id = pro.category_id
+JOIN sales.stores sto ON sto.store_id = ord.store_id
+JOIN sales.staffs sta ON sta.staff_id = ord.staff_id
+GROUP BY
+	ord.order_id,
+	CONCAT(cus.first_name, ' ', cus.last_name),
+	cus.city,
+	cus.state,
+	ord.order_date,
+	pro.product_name,
+	cat.category_name,
+	sto.store_name,
+	CONCAT(sta.first_name, ' ', sta.last_name)
